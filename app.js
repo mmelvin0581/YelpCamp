5ccca821ceac8ba3ae71fc08f0ff304417d31025
Michael Melvin <mmelvin0581@gmail.com>
2018-03-22 23:05:02 -0400

    created user associations for comments

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+
+// seedDB(); // seed the database
 
 /**
  * Passport config
